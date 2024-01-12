d2bf7281697c1d3a064c1bed1db59e2708eecc47
admin <admin@ming-ders-MacBook.local>
2024-01-12 16:06:00 +0800

    blockNumber ok

--- index.tsx
+++ index.tsx
@@ -1,24 +1,28 @@
-import { renderToReadableStream } from "react-dom/server";
-import UsersList from "./ui/components/UsersList";
-
-const server = Bun.serve({
-  port: 3000,
+import { createPublicClient, http, formatUnits } from 'viem'
+import { mainnet } from 'viem/chains'
+ 
+const client = createPublicClient({ 
+  chain: mainnet,
+  transport: http()
+})
+ 
+export default {
   async fetch(req) {
-    const response = await fetch("https://api.github.com/users", {
-      method: "GET",
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const users = await response.json();
-
-    const stream = await renderToReadableStream(<UsersList users={users} />);
-    return new Response(stream, {
-      headers: { "Content-Type": "text/html" },
-    });
+const client = createPublicClient({ 
+  chain: mainnet,
+  transport: http()
+})
+const blockNumber = await client.getBlockNumber() 
+    const path = "./package.json";
+    const file = Bun.file(path);
+    const contents = await file.json();
+    return new Response(JSON.stringify({"blockNumber": formatUnits(blockNumber,0)}),
+    {
+       status: 200,
+       headers: {
+         "Content-type": file.type 
+       },
+    }
+    );
   },
-});
-
-
-console.log(`Server started on PORT:${server.port}`);
+};
